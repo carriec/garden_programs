611d2e110fce2132df78e087ea8cfdaa4695e40b
Carrie Chennault <cchennault@gmail.com>
2019-09-23 13:08:06 -0600

    Install dev version of icpsrdata. Install packages googledrive and dplyr.

--- scripts/install_packages.R
+++ scripts/install_packages.R
@@ -1,1 +1,7 @@
-install.packages("icpsrdata")
+#Use development version of icpsrdata package until updated in CRAN
+install.packages("devtools")
+library(devtools)
+install_github("fsolt/icpsrdata")
+
+#Install packages from CRAN
+install.packages(c("googledrive", "dplyr"))
