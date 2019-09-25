d4f5ee9a35970d3b9396d8c6523e4d334b7c4031
Carrie Chennault <cchennault@gmail.com>
2019-09-25 12:28:54 -0600

    Install 'tidycensus' package. Added notes for required brew install dependencies. Cleaned up code.

--- scripts/install_packages.R
+++ scripts/install_packages.R
@@ -1,8 +1,14 @@
-#Use development version of icpsrdata package until updated in CRAN
+#Packages for downloading ICSPR data: Use development version of icpsrdata package until updated in CRAN
 install.packages("devtools")
 library(devtools)
 install_github("fsolt/icpsrdata")
 
-#Install packages from CRAN
-install.packages(c("googledrive", "tidyverse", "readxl"))
+#Packages for downloading food production data Excel spreadsheets from Google Drive
 ###install.packages("dplyr") -- included in tidyverse
+install.packages(c("googledrive", "readxl", "tidyverse"))
+
+#Packages for analysis with census data
+###if R installed through homebrew, run the following two commands in the terminal:
+###brew install gdal
+###brew install pkg-config
+install.packages("tidycensus")
