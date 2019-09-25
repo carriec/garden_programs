dc06c91dd7c4e659b91ff077fb7037fae16e73b6
Carrie Chennault <cchennault@gmail.com>
2019-09-25 12:35:01 -0600

    Update script to download Excel (.xlsx) workbook with Becca's state and county data.

--- scripts/download_foodproduction_data.R
+++ scripts/download_foodproduction_data.R
@@ -13,5 +13,3 @@ drive_download(
       file = as_id("https://docs.google.com/spreadsheets/d/1Z3bVdLkiYJFrm4UA4lZwftrOLqdRhd0-/"), 
       path = "./data/raw_data/Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER.xlsx",
       overwrite = TRUE)
-Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER_state <- read_excel("./data/raw_data/Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER.xlsx")
-Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER_county <- read_excel("./data/raw_data/Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER.xlsx", sheet = 2)
