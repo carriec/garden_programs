7dd611d486afe88b7cced67dc39f5901f02cbf06
Carrie Chennault <cchennault@gmail.com>
2019-09-25 21:36:19 -0600

    Add state abbreviations to county facility data using code key from ICPSR documentation: 36128-0001-Codebook.pdf.

--- scripts/download_icpsr_data.R
+++ scripts/download_icpsr_data.R
@@ -1,5 +1,16 @@
 #library
 library(icpsrdata)
+library(googledrive)
+library(tidyverse)
+library(readxl)
+
 
 #download data
 icpsr_download(file_id = c(36128, 24642, 37135), download_dir = "data/icpsr_data")
+
+#download state codes in FACID for county jail data (included in 36128-0001-Codebook.pdf)
+state_codes_facid <- drive_get(as_id("https://docs.google.com/spreadsheets/d/1LN0wMtGDgpKqedDBgnTL6d_35KFOOnX9Qnck_bheAc4/"))
+drive_download(
+  file = as_id("https://docs.google.com/spreadsheets/d/1LN0wMtGDgpKqedDBgnTL6d_35KFOOnX9Qnck_bheAc4"), 
+  path = "./data/raw_data/36128-0001-Codebook-State-Codes-Key",
+  overwrite = TRUE)
--- scripts/eda.R
+++ scripts/eda.R
@@ -2,10 +2,20 @@
 
 #library
 library(tidycensus)
+library(tidyverse)
+library(tigris)
 
 #set Census API key, obtained at: http://api.census.gov/data/key_signup.html
 ###census_api_key("INSERT_KEY_HERE", install = TRUE)
 
-#view imported food production data
+#Read in imported food production data
 Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER_state <- read_excel("./data/raw_data/Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER.xlsx")
-Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER_county <- read_excel("./data/raw_data/Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER.xlsx", sheet = 2)
+Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER_county <- read_excel("./data/raw_data/Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER.xlsx", sheet = 2, col_names = FALSE)
+Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER_state <- Correctional_Facility_Ag_Hort_Garden_Becca_States_SUMMER_state[1:106,]
+
+#Match imported State Codes in FACID data with JURISID fields
+da36128.0001_Codebook_State_FACID <- read_excel("./data/raw_data/36128-0001-Codebook-State-Codes-Key.xlsx")
+da36128.0001 %>%
+  mutate(state_code = as.numeric(substr(as.character(JURISID), 1, 2)))
+
+fips_codes <- data(fips_codes)
